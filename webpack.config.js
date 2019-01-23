b0b1faa4a630fc4c5aa9ac81846d57d21c515c83
Neal Bharucha <nealbharucha@yahoo.com>
2019-01-23 11:07:49 -0600

    webpack plugin so dist generates index.html file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
@@ -33,5 +34,8 @@ module.exports = {
     publicPath: 'http://localhost:3000/dist/',
     hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ title: "Neal's Website" }),
+  ],
 };
