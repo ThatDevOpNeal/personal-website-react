bea8e838eb440d469e9368806c0072da0863ab09
Neal Bharucha <nealbharucha@yahoo.com>
2019-02-13 10:17:46 -0600

    add Particles to background

--- components/App.jsx
+++ components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
+import Particles from 'react-particles-js';
 import Title from './Title';
 import Navbar from './Navbar';
 import Home from './Home';
@@ -11,26 +12,44 @@ import Contact from './Contact';
 import '../public/static/css/style.css';
 import '../public/static/css/animations.css';
 
+const particlesParams = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
 const App = () => (
   <HashRouter>
-    <div className="main-container">
-      <link
-        href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
-        rel="stylesheet"
+    <React.Fragment>
+      <Particles
+        params={particlesParams}
+        className="particle-container"
       />
-      <Navbar />
-      <div className="half-container mobile-title">
-        <Title />
+      <div className="main-container">
+        <link
+          href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+          rel="stylesheet"
+        />
+        <Navbar />
+        <div className="half-container mobile-title">
+          <Title />
+        </div>
+        <div className="half-container mobile-container">
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} exact />
+            <Route path="/contact" component={Contact} exact />
+            <Route component={Home} />
+          </Switch>
+        </div>
       </div>
-      <div className="half-container mobile-container">
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} exact />
-          <Route path="/contact" component={Contact} exact />
-          <Route component={Home} />
-        </Switch>
-      </div>
-    </div>
+    </React.Fragment>
   </HashRouter>
 );
 
