ff391f2ac6b618a4bad46b91d57f7fd2c5cc5da7
Neal Bharucha <nealbharucha@yahoo.com>
2019-02-22 01:35:14 -0600

    Modal

---
+++ const/skill.js
@@ -0,0 +1,4 @@
+export const FRONT_END = 'Javascript, ES6, React, Next, HTML, CSS';
+export const BACK_END = 'NodeJS, ExpressJs, Python, APIs, SQL, Postgres';
+export const APPS = 'Swift, React-Native';
+export const CLOUD = 'AWS Certified Developer';
